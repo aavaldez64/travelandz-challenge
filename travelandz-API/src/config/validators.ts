bd57f340ca34308b4a24a911e134bfeb71bfa576
Aarón Valdez <aaronvaldez2001@gmail.com>
2024-03-14 04:15:41 -0400

    add regex to check date YYYY-MM-DD in validators

--- travelandz-API/src/config/validators.ts
+++ travelandz-API/src/config/validators.ts
@@ -7,4 +7,8 @@ export class Validators {
     const phoneRegex = /^\+?([0-9]{1,2})\)?([0-9]{10})$/;
     return phoneRegex.test(value);
   }
+  static isDateYYYYMMDD(value: string) {
+    const dateRegex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
+    return dateRegex.test(value);
+  }
 }
